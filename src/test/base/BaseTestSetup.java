99d16a803f91d7ca5cc3839107b1db0af65b5dd9
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-03-16 16:11:03 -0300

    Added log4j

--- src/test/base/BaseTestSetup.java
+++ src/test/base/BaseTestSetup.java
@@ -4,23 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.log4testng.Logger;
 
 public class BaseTestSetup {
 
     private static final String RESOURCES_PATH = "src/test/resources/";
+    private static final String BASE_URL = "https://www.appdirect.com/";
+
+    public static Logger logger = Logger.getLogger(BaseTestSetup.class);
+
     private WebDriver driver;
     private ChromeOptions chromeOptions;
 
-    @BeforeSuite
-    public void setUpBrowser(){
+    @BeforeSuite(alwaysRun = true)
+    public void setUpBrowser(ITestContext context){
         setChromeOptions();
+        logger.info("Starting Chrome Browser...");
         driver = new ChromeDriver(chromeOptions);
     }
 
     private void setChromeOptions(){
+        logger.info("Setting up Chrome Browser");
         System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver");
         chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("test-type", "start-maximized", "no-default-browser-check");
@@ -28,8 +36,9 @@ public class BaseTestSetup {
     }
 
     @BeforeMethod(alwaysRun = true)
-    private void openBasePage(){
-        driver.navigate().to("https://www.appdirect.com/");
+    public void openBasePage(){
+        logger.info("Navigating to Base URL: " + BASE_URL);
+        driver.navigate().to(BASE_URL);
     }
 
     @AfterMethod(alwaysRun = true)
--- src/test/tests/LoginTests.java
+++ src/test/tests/LoginTests.java
@@ -1,12 +1,15 @@
 package tests;
 
 import base.BaseTestSetup;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseTestSetup{
 
+    @BeforeMethod(alwaysRun = true)
+
     @Test
     public void simpleLoginTest(){
-        
+
     }
 }
