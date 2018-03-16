57a2568833436e3b3812ee2eb69228d0187703ae
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-03-16 16:44:42 -0300

    Addded test ng logger

--- src/test/base/BaseTestSetup.java
+++ src/test/base/BaseTestSetup.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.log4testng.Logger;
+
 
 public class BaseTestSetup {
 
@@ -19,6 +21,15 @@ public class BaseTestSetup {
 
     private WebDriver driver;
     private ChromeOptions chromeOptions;
+
+    @BeforeClass
+    public void prepareLogger() throws Exception
+    {
+        Class<? extends BaseTestSetup> currTestClass = getClass();
+        this.logger = Logger.getLogger(currTestClass);
+        String message = "Started executing class " + this.getClass().getName();
+        logger.info(message);
+    }
 
     @BeforeSuite(alwaysRun = true)
     public void setUpBrowser(ITestContext context){
